85fb15cab0bd3479a127e3e19a715f1bc1d0ca70
Enes <81771019+Maglizone@users.noreply.github.com>
2021-04-19 01:05:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const { play, stop } = require('./commands');
 
 const bot = new Client();
 
-bot.login('process.env.token');
+const token = ('process.env.token');
 
 bot.on('ready', () => console.log('ErdoğanBotu Aktif'));
 
